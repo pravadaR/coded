dc6f47f19400b96b8d230cc82f44d16db50f62a1
pravadaR <pravada290407@gmail.com>
2026-06-07 22:34:08 +0530

    Add files via upload

---
+++ qr_code.py
@@ -0,0 +1,7 @@
+import qrcode
+website_link='https://www.ananyguru.com'
+qr=qrcode.QRCode(version=1, box_size=5, border=5)
+qr.add_data(website_link)
+qr.make()
+img=qr.make_image(fill_color='black',back_color='white')
+img.save('ananyguru_qr.png')
